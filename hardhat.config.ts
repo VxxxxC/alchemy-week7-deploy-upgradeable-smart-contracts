90336f4cf0b6e218283f66f30f38760c72dbafc3
vxxxxc <>
2025-02-03 04:37:34 +0800

    fix npm dependencies version conflict

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,23 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomiclabs/hardhat-etherscan";
+import "@openzeppelin/hardhat-upgrades";
+import dotenv from "dotenv";
+
+const process = dotenv;
+const env = process.config().parsed!;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  networks: {
+    sepolia: {
+      url: env.ALCHEMY_SEPOLIA_URL,
+      accounts: [env.WALLET_PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: env.ETHERSCAN_KEY,
+  },
 };
 
 export default config;
